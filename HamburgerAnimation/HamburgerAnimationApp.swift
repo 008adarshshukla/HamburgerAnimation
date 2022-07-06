31a404f40767877bd752cd94aa1e11750f3d4918
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-07-06 14:48:47 +0530

    Initial Commit

---
+++ HamburgerAnimation/HamburgerAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamburgerAnimationApp.swift
+//  HamburgerAnimation
+//
+//  Created by Adarsh Shukla on 06/07/22.
+//
+
+import SwiftUI
+
+@main
+struct HamburgerAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
